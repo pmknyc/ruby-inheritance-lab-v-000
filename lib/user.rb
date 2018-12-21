a59d6567a9bb075034baae2bd05c20a3e26bab6e
Paula McKinley <pmknyc@optimum.net>
2018-12-21 01:07:27 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,7 +3,7 @@ class User
   attr_accessor :first_name, :last_name
 
   def new_student(obj_name,first_name,last_name)
-    obj_name = User.new(first_name, last_name)
+    obj_name = Student.new(first_name, last_name)
   end
 
 end  #class end
