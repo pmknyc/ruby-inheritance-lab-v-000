3b401a313b8360fb944d233312e99bfbc6dab0a3
Paula McKinley <pmknyc@optimum.net>
2018-12-21 01:10:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ attr_accessor :knowledge
   def new_student(obj_name,first_name,last_name)
     obj_name = Student.new(first_name, last_name)
   end
-  
+
   def learn(string)
     @knowledge << string
   end
