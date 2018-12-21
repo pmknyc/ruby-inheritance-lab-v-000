ab2ae47d8cd79edd7ab9499992447ca894519dda
Paula McKinley <pmknyc@optimum.net>
2018-12-21 01:09:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,16 @@ class Student < User
 
 attr_accessor :knowledge
 
-  def initialize
+  def initialize(first_name, last_name)
     @knowledge = []
+    @first_name = first_name
+    @last_name = last_name
   end
 
+  def new_student(obj_name,first_name,last_name)
+    obj_name = Student.new(first_name, last_name)
+  end
+  
   def learn(string)
     @knowledge << string
   end
