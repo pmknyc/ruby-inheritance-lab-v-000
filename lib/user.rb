c1452ccf6315a5839d8dbc7a1f01e0fcb14cc70f
Paula McKinley <pmknyc@optimum.net>
2018-12-21 01:02:20 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,12 +2,14 @@ class User
 
   attr_accessor :first_name, :last_name
 
+
   jim = User.new
   jim.first_name = "Jim"
   jim.last_name = "Jones"
   jim.learn(some_knowledge)
 
-  def new_user
+  def new_user(obj_name,first_name,last_name)
+    obj_name = User.new(first_name, last_name)
   end
 
 
